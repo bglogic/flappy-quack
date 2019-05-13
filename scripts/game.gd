015525f612ee9884df1fc1cb59730425a2c3f957
Saif Almansoori <smm.almansoori@mailbox.org>
2019-05-13 05:08:25 +0400

    HUD and state manager

--- scripts/game.gd
+++ scripts/game.gd
@@ -10,6 +10,12 @@ var score_current: int = 0 setget _set_score_current
 signal score_best_changed
 signal score_current_changed
 
+func _ready() -> void:
+  stage_manager.connect("stage_changed", self, "_on_stage_changed")
+
+func _on_stage_changed() -> void:
+  score_current = 0
+
 func _set_score_best(new_value: int) -> void:
   score_best = new_value
   emit_signal("score_best_changed")
---
+++ scripts/stage_manager.gd
@@ -0,0 +1,30 @@
+extends CanvasLayer
+
+const STAGE_GAME: String = "res://stages/game_stage.tscn"
+
+var is_changing: bool = false
+
+signal stage_changed
+
+func _get_is_changing() -> bool:
+  return is_changing
+
+func change_scene(stage_path: String) -> void:
+  if is_changing: return
+  
+  layer = 5
+  is_changing = true
+  get_tree().get_root().set_disable_input(true)
+  
+  get_node("anim").play("fade_in")
+  yield(get_node("anim"), "animation_finished")
+  
+  get_tree().change_scene(stage_path)
+  emit_signal("stage_changed")
+  
+  get_node("anim").play("fade_out")
+  yield(get_node("anim"), "animation_finished")
+  
+  layer = 1
+  is_changing = false
+  get_tree().get_root().set_disable_input(false)
