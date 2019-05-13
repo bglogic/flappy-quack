80c9ca7f967308e5c836259bfa9c06c230182439
Saif Almansoori <smm.almansoori@mailbox.org>
2019-05-13 07:02:50 +0400

    HUD and scores

--- scripts/game.gd
+++ scripts/game.gd
@@ -17,8 +17,9 @@ func _on_stage_changed() -> void:
   score_current = 0
 
 func _set_score_best(new_value: int) -> void:
-  score_best = new_value
-  emit_signal("score_best_changed")
+  if new_value > score_best:
+    score_best = new_value
+    emit_signal("score_best_changed")
 
 func _set_score_current(new_value: int) -> void:
   score_current = new_value 
